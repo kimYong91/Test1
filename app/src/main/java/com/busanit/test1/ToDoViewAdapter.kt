59090fda6cc173689361f01f577bcaa57caf95b0
KimYong91 <rladyd97@naver.com>
2024-05-24 12:22:02 +0900

    Ex1

--- app/src/main/java/com/busanit/test1/ToDoViewAdapter.kt
+++ app/src/main/java/com/busanit/test1/ToDoViewAdapter.kt
@@ -4,27 +4,25 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.busanit.test1.databinding.TodoBinding
 
-lateinit var intent: Intent
+
 class ToDoViewAdapter(val toDoList: MutableList<ToDo>) : RecyclerView.Adapter<ToDoViewAdapter.ToDoHolder>(){
 
     inner class ToDoHolder(val binding: TodoBinding) : RecyclerView.ViewHolder(binding.root) {
         var toDoText: TextView = binding.toDoText
-
+        val toDoBind = binding.root.context
 
         fun bind() {
-
             binding.deleteButton.setOnClickListener {
                 val position = adapterPosition
                 toDoList.removeAt(position)
                 notifyItemRemoved(position)
             }
             binding.linearView.setOnClickListener {
-                intent = Intent(binding.root.context, DetailActivity::class.java)
-
+                val intent = Intent(toDoBind, DetailActivity::class.java)
+                toDoBind.startActivity(intent)
             }
         }
     }
